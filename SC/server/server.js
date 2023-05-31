475a7083838db847f1fecaa315a29e3f8a6150c8
odczik <58082541+odczik@users.noreply.github.com>
2023-05-31 14:56:23 +0200

    Create server.js

---
+++ SC/server/server.js
@@ -0,0 +1,15 @@
+const https = require('https');
+const ws = require('ws');
+
+let server = https.createServer(options, (req, res) => {
+  res.sendStatus(200);
+});
+server.addListener('upgrade', (req, res, head) => console.log('UPGRADE:', req.url));
+server.on('error', (err) => console.error(err));
+server.listen(8080, () => console.log('Https running on port 8080'));
+
+const wss = new ws.Server({server, path: '/echo'});
+wss.on('connection', function connection(ws) {
+    ws.send('Hello');   
+    ws.on('message', (data) => ws.send('Receive: ' + data));
+});
