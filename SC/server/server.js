78fe9ed981eae7f8c593de16bcce6ae5fe656d80
odczik <ondrej.dostal007@gmail.com>
2023-05-31 16:22:31 +0200

    update

--- SC/server/server.js
+++ SC/server/server.js
@@ -1,13 +1,15 @@
-const https = require('https');
+const http = require('http');
 const ws = require('ws');
 
-let server = https.createServer({}, (req, res) => {
+let server = http.createServer((req, res) => {
   res.writeHead(200);
   res.end(`hello world\n`);
 });
 server.addListener('upgrade', (req, res, head) => console.log('UPGRADE:', req.url));
 server.on('error', (err) => console.error(err));
 server.listen(8080, () => console.log('Https running on port 8080'));
+
+
 
 const wss = new ws.Server({server, path: '/echo'});
 wss.on('connection', function connection(ws) {
