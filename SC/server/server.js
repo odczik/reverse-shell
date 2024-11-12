710a30141c058e9434452fc5fd0fdc4dafdab8b5
odczik <58082541+odczik@users.noreply.github.com>
2024-11-12 21:44:36 +0100

    Update server.js

--- SC/server/server.js
+++ SC/server/server.js
@@ -39,7 +39,7 @@ wss.on('connection', function connection(ws) {
         try {
             msg = JSON.parse(msg.data);
         } catch(e) {
-            console.error(e.message);
+            console.error(e);
             return ws.terminate();
         }
         switch(msg.type){
